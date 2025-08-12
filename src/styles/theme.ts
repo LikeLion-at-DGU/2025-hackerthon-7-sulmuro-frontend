fe47291bccf4dc363686b9e5c1f75d83f2aa4bcc
sem201 <sem201@naver.com>
2025-08-12 23:08:17 +0900

    feat : 글로벌 테마 설정

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -14,91 +14,64 @@ const fontGenerator = (
 
 const colors = {
   // 배경색
-  Bg: "#FAFAFA",
+  Bg: "#FFF",
 
-  // 포인트 색상
-  Point: "#FFD232",
+  // Netral
+  N70: "#2A2A2E",
+  N50: "#52525B",
+  N40: "#71717A",
+  N30: "#A1A1AA",
+  N20: "#D4D4D8",
+  N10: "#E4E4E7",
+  N00: "#F4F4F5",
+  WHITE: "#FFF",
 
-  // 메인 색상
-  Orange01: "#FF6E3F",
-  Orange02: "#FFF0EC",
-  Gray01: "#F2F2F2",
+  // 파랑 색상
+  B60: "#0C3CB7",
+  B50: "#4278EA",
+  B40: "#72A1FF",
+  B30: "#A2C7FF",
+  B20: "#DAE9FF",
+  B10: "#ECF4FF",
 
-  // 텍스트 색상
-  Black01: "#2A2A2A",
-  Black02: "#C0C0C0",
-
-  // 기타 색상
-  White: "#FFFFFF",
-  Black: "#000000",
+  // 빨강 색상
+  R60: "#AD0002",
+  R50: "#FC696A ",
+  R40: "#FF9596",
+  R30: "#FFB5BB",
+  R20: "#FFD9D9",
+  R10: "#FFF1F1 ",
 };
 
 const theme = {
   colors,
 
   fonts: {
-    // Title
-    ExtraBold24: fontGenerator(
-      "SUIT-ExtraBold",
-      "24px",
-      "800",
-      "auto",
-      "normal"
-    ),
-    Bold24: fontGenerator("SUIT-Bold", "24px", "700", "auto", "normal"),
-    ExtraBold20: fontGenerator(
-      "SUIT-ExtraBold",
-      "20px",
-      "800",
-      "auto",
-      "normal"
-    ),
-    Bold20: fontGenerator("SUIT-Bold", "20px", "700", "auto", "normal"),
-    ExtraBold18: fontGenerator(
-      "SUIT-ExtraBold",
-      "18px",
-      "800",
-      "auto",
-      "normal"
-    ),
-    Bold18: fontGenerator("SUIT-Bold", "18px", "700", "auto", "normal"),
+    // Head1
+    ExtraBold24: fontGenerator("Pretendard", "24px", " 800", "auto", "130%"),
+    Bold24: fontGenerator("Pretendard", "24px", "700", "auto", "130%"),
 
-    // SubTitle
-    ExtraBold16: fontGenerator(
-      "SUIT-ExtraBold",
-      "16px",
-      "800",
-      "auto",
-      "normal"
-    ),
-    Bold16: fontGenerator("SUIT-Bold", "16px", "700", "auto", "normal"),
-    SemiBold16: fontGenerator("SUIT-SemiBold", "16px", "600", "auto", "normal"),
-    Medium16: fontGenerator("SUIT-Medium", "16px", "500", "auto", "normal"),
+    // Head2
+    Bold20: fontGenerator("Pretendard", "20px", "700", "auto", "130%"),
+    SemiBold20: fontGenerator("Pretendard", "20px", "600", "auto", "130%"),
 
-    // Body
-    ExtraBold14: fontGenerator(
-      "SUIT-ExtraBold",
-      "14px",
-      "800",
-      "auto",
-      "normal"
-    ),
-    Bold14: fontGenerator("SUIT-Bold", "14px", "700", "auto", "normal"),
-    SemiBold14: fontGenerator("SUIT-SemiBold", "14px", "600", "auto", "normal"),
-    Medium14: fontGenerator("SUIT-Medium", "14px", "500", "auto", "normal"),
+    // Body1
+    Bold16: fontGenerator("Pretendard", "16px", "700", "auto", "normal"),
+    SemiBold16: fontGenerator("Pretendard", "16px", "600", "auto", "normal"),
+    Regular16: fontGenerator("Pretendard", "16px", "400", "auto", "normal"),
 
-    // Caption
-    ExtraBold12: fontGenerator(
-      "SUIT-ExtraBold",
-      "12px",
-      "800",
-      "auto",
-      "normal"
-    ),
-    Bold12: fontGenerator("SUIT-Bold", "12px", "700", "auto", "normal"),
-    SemiBold12: fontGenerator("SUIT-SemiBold", "12px", "600", "auto", "normal"),
-    Medium12: fontGenerator("SUIT-Medium", "12px", "500", "auto", "normal"),
-    SemiBold10: fontGenerator("SUIT-SemiBold", "10px", "600", "auto", "normal"),
+    // Body2
+    SemiBold14: fontGenerator("Pretendard", "14px", "600", "auto", "normal"),
+    Regular14: fontGenerator("Pretendard", "14px", "400", "auto", "normal"),
+
+    // small1
+    SemiBold12: fontGenerator("Pretendard", "12px", "600", "auto", "normal"),
+    Regular12: fontGenerator("Pretendard", "12px", "400", "auto", "normal"),
+
+    // small2
+    // Body2
+    SemiBold10: fontGenerator("Pretendard", "10px", "600", "auto", "normal"),
+    Regular10: fontGenerator("Pretendard", "10px", "400", "auto", "normal"),
   },
 };
 
