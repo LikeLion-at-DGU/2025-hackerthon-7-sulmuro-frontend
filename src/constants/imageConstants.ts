6b44a1fd1601a0c37b1593a86d8d56255f175276
sem201 <sem201@naver.com>
2025-08-15 16:22:04 +0900

    feat : footer 디자인 생성

---
+++ src/constants/imageConstants.ts
@@ -0,0 +1,14 @@
+// footer icon
+import ArticleUnselected from "@/assets/icons/article_unselect.svg";
+import MapUnselected from "@/assets/icons/map_unselect.svg";
+import SavedUnselected from "@/assets/icons/saved_unselect.svg";
+import TalkUnselected from "@/assets/icons/talk_unselect.svg";
+import SearchAI from "@/assets/icons/search_ai.svg";
+
+export const IMAGE_CONSTANTS = {
+  ArticleUnselected,
+  MapUnselected,
+  SavedUnselected,
+  TalkUnselected,
+  SearchAI,
+};
