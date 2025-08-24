5b85b5a48cef131d956fa89f50c7dd25466b8fc8
sem201 <sem201@naver.com>
2025-08-25 00:45:28 +0900

    feat : 저장된 장소나 아티클이 없을 경우 문구 추가

--- src/pages/saved/SavedPage.tsx
+++ src/pages/saved/SavedPage.tsx
@@ -11,37 +11,64 @@ import SavedArticleCard from "./_components/SavedArticleCard";
 import { savedPlaces, savedArticles } from "./dummy/dummyData";
 import { useState } from "react";
 import { Article } from "../article/_apis/getArticle";
+import { useLanguage } from "@/components/contexts/LanguageContext";
+import {
+  SavedPageArticleEmptyCase,
+  savedPageArticleTitle,
+  SavedPagePlaceEmptyCase,
+  SavedPagePlaceTitle,
+} from "../map/languages/Translate";
 const SavePage = () => {
   const [_markedPlaces, _setMarkedPlaces] = useState<Article[]>();
+  const { language } = useLanguage();
   // const sfetchData = () => {};
+
   return (
     <S.Wrapper>
       <S.Header>Saved</S.Header>
       <S.Contents>
-        <MainHeader text="장소" path={ROUTE_PATHS.SAVED_PLACE} />
-        <S.SavedPlaceBox>
-          {savedPlaces.slice(0, 2).map((place) => (
-            <SavePlaceCard
-              key={place.id}
-              id={place.id}
-              name={place.name}
-              path={`${ROUTE_PATHS.MAP}?place=${place.id}`}
-              thumbnailUrl={place.thumbnailUrl}
-              address={place.address}
-            />
-          ))}
+        <MainHeader
+          text={SavedPagePlaceTitle[language]}
+          path={ROUTE_PATHS.SAVED_PLACE}
+        />
+        <S.SavedPlaceBox isEmpty={savedPlaces.length === 0}>
+          {savedPlaces.length === 0 ? (
+            <S.EmptyBox>{SavedPagePlaceEmptyCase[language]}</S.EmptyBox>
+          ) : (
+            savedPlaces
+              .slice(0, 2)
+              .map((place) => (
+                <SavePlaceCard
+                  key={place.id}
+                  id={place.id}
+                  name={place.name}
+                  path={`${ROUTE_PATHS.MAP}?place=${place.id}`}
+                  thumbnailUrl={place.thumbnailUrl}
+                  address={place.address}
+                />
+              ))
+          )}
         </S.SavedPlaceBox>
-        <MainHeader text="아티클" path={ROUTE_PATHS.SAVED_ARTICLE} />
+        <MainHeader
+          text={savedPageArticleTitle[language]}
+          path={ROUTE_PATHS.SAVED_ARTICLE}
+        />
         <S.SavedArticleBox>
-          {savedArticles.slice(0, 1).map((article) => (
-            <SavedArticleCard
-              key={article.id}
-              id={article.id}
-              title={article.title}
-              images={article.images}
-              location={article.location}
-            />
-          ))}
+          {savedArticles.length === 0 ? (
+            <S.EmptyBox>{SavedPageArticleEmptyCase[language]}</S.EmptyBox>
+          ) : (
+            savedArticles
+              .slice(0, 1)
+              .map((article) => (
+                <SavedArticleCard
+                  key={article.id}
+                  id={article.id}
+                  title={article.title}
+                  images={article.images}
+                  location={article.location}
+                />
+              ))
+          )}
         </S.SavedArticleBox>
       </S.Contents>
     </S.Wrapper>
